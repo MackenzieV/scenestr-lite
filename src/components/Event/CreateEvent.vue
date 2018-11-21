ad8e67cb9bb085d4ce124c1cc331552b3dc7302d
Mackenzie V <lonesomegeorge87@gmail.com>
2018-11-21 13:32:19 -0600

    wiring up back-end with front-end

--- src/components/Event/CreateEvent.vue
+++ src/components/Event/CreateEvent.vue
@@ -1,21 +1,19 @@
 <template>
 <div id="createEvent">
-
-    <div class="descText">Event Details:</div>
+  <div class="descText">Event Details:</div>
     <v-flex xs12 sm12 md12>
-      <v-text-field v-for="item in eventList" :key="item.list" label="Solo" :placeholder="item.list" solo></v-text-field>
+      <v-text-field v-model="event.list" v-for="item in eventList" :key="item.list" label="Solo" :placeholder="item.list" solo></v-text-field>
     </v-flex>
-
-    <div class="descText">Description:</div>
+  <div class="descText">Description:</div>
     <v-flex xs12>
-     <v-textarea solo name="input-7-4" label="Solo textarea" value="This is the event description"></v-textarea>
+      <v-textarea solo name="input-7-4" label="Solo textarea" value=""></v-textarea>
     </v-flex>
     <div class="descText">Keywords:</div>
     <v-flex xs12 sm12 md12>
       <v-text-field label="Solo" placeholder="e.g. Karaoke, Games, Beer, etc." solo></v-text-field>
     </v-flex>
-    <v-btn class="btn">Publish</v-btn>
-
+    <v-btn @click="addEvent(newEvent)"class="btn1">Publish</v-btn>
+    <v-btn class="btn2">Clear</v-btn>
 </div>
 </template>
 
@@ -23,17 +21,42 @@
 export default {
   name: 'createEvent',
   data () {
+    data: {
+      pageTitle:'Events',
+      userEvents [],
+      newEvent: {
+        userId: 0,
+        title: '',
+        date:'',
+        location: '',
+        cost: '',
+        description: '',
+        keywords: ''
+        }
+    },
     return {
       eventList: [
-        { list: 'User ID' },
+        { list: 'Event ID' },
         { list: 'Event Name' },
         { list: 'Location' },
         { list: 'Date/Time'},
         { list: 'Cost' },
       ]
     }
+    methods:{
+    addEvent: function (event) {
+      axios.post('http://127.0.0.1:3000/add-event/', event)
+        .then(function (response) {
+          console.log(response.data[0]);
+        })
+        .catch(function (error) {
+          console.log(error);
+        })
+      }
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -52,13 +75,22 @@ export default {
 v-textarea {
   height:5em;
 }
-.btn {
+.btn1 {
   display: block;
   margin-left: auto;
   margin-right: auto;
+  position: relative;
+  right: 4em;
 }
-.imgFile {
-
+.btn2 {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  position: relative;
+  bottom:3em;
+  left: 4em;
 }
-
+.v-navigation-drawer {
+  position: absolute;
+}
 </style>
